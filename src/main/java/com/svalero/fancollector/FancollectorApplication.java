b5d41d3a3122fbe4852cd7a525d037edbabcf16a
NereaTM <nerea.azuara@gmail.com>
2025-12-09 18:30:00 +0100

    first commit

---
+++ src/main/java/com/svalero/fancollector/FancollectorApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.fancollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FancollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/fancollector/FancollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.fancollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
