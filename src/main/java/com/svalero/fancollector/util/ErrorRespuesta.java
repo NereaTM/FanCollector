73cf3fac7c21ad1e8114ef9efdb7b95acff6391f
NereaTM <nerea.azuara@gmail.com>
2025-12-23 23:05:35 +0100

    manejo global de errores y validaciones

---
+++ src/main/java/com/svalero/fancollector/util/ErrorRespuesta.java
@@ -0,0 +1,26 @@
+package com.svalero.fancollector.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorRespuesta {
+    private int statusCode;
+    private String message;
+    private Map<String, String> errors = new HashMap<>();
+
+    public ErrorRespuesta(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = new HashMap<>();
+    }
+
+    public static ErrorRespuesta error500(String message) {return new ErrorRespuesta(500, message);}
+
+}
